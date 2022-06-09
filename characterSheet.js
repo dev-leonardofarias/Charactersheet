a0f72f2dfe695b705325c9a206a31a4c7eee4981
Leonardo Farias <leonbr101@gmail.com>
2022-06-08 23:17:34 -0300

    Adicionando os Status

---
+++ characterSheet.js
@@ -0,0 +1,19 @@
+const data = {
+    name: 'Jaime Orthuga',
+    player: "",
+    occupation: "Ocultista / Agente",
+    age: 29,
+    sex: 'male',
+    birthplace: "",
+    residence: "São Paulo",
+
+    life: {
+        current: 10,
+        max: 12,
+    },
+
+    sanity: {
+        current: 50,
+        max: 60,
+    },
+}
